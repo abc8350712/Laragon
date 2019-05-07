4fa4bce0095227db62ebd31e17a73ec1be476aa1
Jack <abc8350712@gmail.com>
2019-05-07 23:15:22 +0800

    use blade to define layout

--- www/blog/resources/views/pages/welcome.blade.php
+++ www/blog/resources/views/pages/welcome.blade.php
@@ -1,99 +1,57 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('main')
+@section('content')
+    <div class="container">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="jumbotron">
+            <h1>Welcome to My Blog!</h1>
+            <p class="lead">Thank you so much for visiting. This is my test website built with Laravel. Please read my popular post!</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a></p>
+          </div>
         </div>
-    </body>
-</html>
+      </div>
+      <!-- end of header .row -->
+
+      <div class="row">
+        <div class="col-md-8">
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis amet tenetur eum, consequuntur assumenda officiis quidem omnis placeat. Sequi ex fugiat reiciendis at eligendi inventore ad, odio magnam velit doloribus...</p>
+            <a href="#" class="btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis amet tenetur eum, consequuntur assumenda officiis quidem omnis placeat. Sequi ex fugiat reiciendis at eligendi inventore ad, odio magnam velit doloribus...</p>
+            <a href="#" class="btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis amet tenetur eum, consequuntur assumenda officiis quidem omnis placeat. Sequi ex fugiat reiciendis at eligendi inventore ad, odio magnam velit doloribus...</p>
+            <a href="#" class="btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis amet tenetur eum, consequuntur assumenda officiis quidem omnis placeat. Sequi ex fugiat reiciendis at eligendi inventore ad, odio magnam velit doloribus...</p>
+            <a href="#" class="btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+        </div>
+
+        <div class="col-md-3 col-md-offset-1">
+          <h2>Sidebar</h2>
+        </div>
+      </div>
+
+    </div>
+@endsection
