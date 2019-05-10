91a34688b4921bd85ffa23ad20e9dca2afa0dd32
Jack <abc8350712@gmail.com>
2019-05-11 01:00:01 +0800

    post+connectmysql

--- www/blog/app/Http/Controllers/PostController.php
+++ www/blog/app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class PostController extends Controller
 {
     /**
@@ -36,6 +36,18 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
+		$this->validate($request, array(
+			'title' => 'required|max:255',
+			'body' => 'required'
+		));
+		
+		$post = new Post;
+		$post->title = $request->title;
+		$post->body = $request->body;
+		
+		$post->save();
+		
+		return redirect()->route('post.show', $post->id);
     }
 
     /**
@@ -80,6 +92,6 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
-    }
+        //validate the dataz
+	}
 }
