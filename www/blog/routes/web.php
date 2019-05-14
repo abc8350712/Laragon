8e85f0c1b0cac845a5caf00e797b369323ad8dbf
Jack <abc8350712@gmail.com>
2019-05-15 00:10:14 +0800

    Session信息Flash保存

--- www/blog/routes/web.php
+++ www/blog/routes/web.php
@@ -10,8 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('contact', 'PagesController@getContact');
 
-Route::get('/', 'PagesController@getIndex');
-Route::get('about', 'PagesController@getAbout');
-Route::resource('posts', 'PostController');
+
+Route::group(['middleware'  => ['web']], function(){
+	Route::get('contact', 'PagesController@getContact');
+	Route::get('/', 'PagesController@getIndex');
+	Route::get('about', 'PagesController@getAbout');
+	Route::resource('posts', 'PostController');
+});
